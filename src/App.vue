61ccd7976d20d596498c6a2b50e1076098fdfcaf
Alejandro Martín de León <alu0101015941@ull.edu.es>
2021-11-25 23:28:01 +0000

    first config

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-main>
+      <fileRead/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import fileRead from './components/fileRead';
+
+export default {
+  name: 'App',
+
+  components: {
+    fileRead,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/fileRead.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-container>
+       <p>hola</p>
+        <div id="readfile">
+        <input v-on:click="readFile" type="file" name="inputfile" id="inputfile">
+        <pre id="output"></pre>
+    </div>
+
+    <div>
+        Métrica a elegir
+    </div>
+  </v-container>
+</template>
+
+<script>
+  export default {
+
+
+       data() {
+        return {
+            message: 'Hola'
+        }
+    },
+    methods: {
+        // readFile() {
+        //     // var file = new File([""],"../../public/files/file1.txt");
+        //     // var reader = new FileReader();
+        //     // reader.readAsText(file);
+        //     // reader.onload = function() {
+        //     //     console.log(this.result);
+        //     // }
+            
+        // },
+        readFile() {
+            document.getElementById("inputfile").addEventListener("change", function() {
+                var reader = new FileReader();
+                var matrix;
+                var matrix_content;
+                reader.onload = function() {
+                    var lines = this.result.split("\n");
+                    matrix = new Array(lines.length).fill(0).map(() => new Array(lines.length).fill(0));
+                    for(var i = 0; i < lines.length; i++) {
+                        matrix_content = lines;
+                        
+                        matrix_content = this.result.split(/\s+/);
+                        
+                        for (var j = 0; j < (matrix_content.length / lines.length).toFixed(); j++) {
+                            // OJO: AQUI CAMBIO DE STRING A INT
+                            matrix[i][j] = parseInt(matrix_content[((matrix_content.length / lines.length).toFixed() * i) + j]);
+                            console.log((matrix_content.length / lines.length).toFixed());
+                        }
+                    }
+                    
+                    console.log(matrix);
+                    
+                }
+                reader.readAsText(this.files[0]);
+            })
+        },
+        selectMetrics() {
+            
+        }
+    }
+}
+</script>
