1fab6428554d2748a37b6475f8c10d6728b93b8d
Alejandro Martín de León <alu0101015941@ull.edu.es>
2021-11-27 18:24:09 +0000

    Se ha añadido la estructura inicial del switch para la eleccion de metrica

--- src/components/fileRead.vue
+++ src/components/fileRead.vue
@@ -34,7 +34,7 @@
         </v-col>
 
     </div>
-
+      
     <div>
       <p>Tipo de predicción</p>
       <v-row align="center">
@@ -42,6 +42,14 @@
           <v-select :items="itemsPrediction" label="Seleccione la predicción" dense solo v-model="prediction" v-on:change = "selectPrediction"></v-select>
         </v-col>
       </v-row>
+    </div>
+
+    <div>
+      <v-btn
+        color="primary"
+        elevation="2"
+        v-on:click="operate"
+      >Calcular</v-btn>
     </div>
   </v-container>
 </template>
@@ -75,11 +83,13 @@
                 var matrix_content;
                 reader.onload = function() {
                     var lines = this.result.split("\n");
+                    console.log(lines);
                     matrix = new Array(lines.length).fill(0).map(() => new Array(lines.length).fill(0));
                     for(var i = 0; i < lines.length; i++) {
                         matrix_content = lines;
                         
                         matrix_content = this.result.split(/\s+/);
+                        console.log(matrix_content);
                         
                         for (var j = 0; j < (matrix_content.length / lines.length).toFixed(); j++) {
                             // OJO: AQUI CAMBIO DE STRING A INT
@@ -102,6 +112,22 @@
         },
         getNeighbors() {
           console.log(this.neighbor);
+        }, 
+
+        operate() {
+          switch (this.metric) {
+            case 'Correlación de Pearson': 
+              console.log('Ha seleccionado Correlación de Pearson')
+              break;
+            case 'Distancia coseno': 
+              console.log('Ha seleccionado Correlación de coseno')
+              break;
+            case 'Distancia Euclídea': 
+              console.log('Ha seleccionado Correlación de Euclidea')
+              break;
+            default:
+                console.log('ERROR: No se ha seleccionado ninguna Correlación.')
+          }
         }
     }
 }
